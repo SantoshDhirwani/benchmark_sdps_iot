0f48d8a4a37b0e5f8920fb7a01b38fb53113a7bf
Kevin Gu <kevinguemail@gmail.com>
2020-07-15 22:11:09 +0200

    Update AbstractNettyCreatorThread.java
    added option to vary throughput

--- InputGenerator/InputGenerator/src/main/java/de/adrian/thesis/generator/benchmark/netty/creators/AbstractNettyCreatorThread.java
+++ InputGenerator/InputGenerator/src/main/java/de/adrian/thesis/generator/benchmark/netty/creators/AbstractNettyCreatorThread.java
@@ -1,22 +1,22 @@
 package de.adrian.thesis.generator.benchmark.netty.creators;
 
+import com.google.common.util.concurrent.RateLimiter;
 import de.adrian.thesis.generator.benchmark.javaio.CreatorThread;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Abstract creator for the NexmarkBenchmark
  */
 public abstract class AbstractNettyCreatorThread extends Thread {
-
     private static final Logger LOG = LogManager.getLogger(AbstractNettyCreatorThread.class);
-
     private final BlockingQueue<String> queue;
     private final AbstractNettyCreatorThreadProperties properties;
-
     private volatile boolean interrupted = false;
     long counter = 0;
 
@@ -28,30 +28,32 @@ public abstract class AbstractNettyCreatorThread extends Thread {
 
     @Override
     public void run() {
-
+        System.out.println("Starting NettyCreator, parameter msgs/s:" + properties.messagesPerSecond);
+        final int defaultSpeed = properties.messagesPerSecond;  //default throughput msg/s taken from parameter
         // Generate initial numbers
         for (; counter < properties.initialRecords && !interrupted; counter++) {
-
             String record = generateRecord(counter);
-
             queue.add(record);
-
             if (properties.logMessages && counter % properties.logMessagesModulo == 0) {
                 LOG.info("Initially inserted '{}' into queue", record);
             }
         }
-
+        System.out.println("Initial records generated, no: " + queue.size());
+//        int randThroughput = ThreadLocalRandom.current().nextInt(5000, 10000);
+//        RateLimiter throughputThrottler = RateLimiter.create(randThroughput, 5, TimeUnit.SECONDS);
         while (counter < properties.maxNumbers && !interrupted) {
+            // varys throughput 40%+/- base load
+            this.properties.messagesPerSecond = ThreadLocalRandom.current().nextInt((int)(defaultSpeed * ((double)6/10)), (int)(defaultSpeed * ((double)14/10)));
+//            System.out.println("New speed is " + properties.messagesPerSecond);
             long emitStartTime = System.currentTimeMillis();
-
             int i = 0;
             for (; i < properties.messagesPerSecond && counter + i < properties.maxNumbers; i++) {
+//                throughputThrottler.acquire(1);
                 String record = generateRecord(counter + i);
-
                 queue.add(record);
             }
-
             counter += i;
+            System.out.println(counter + " records generated with new speed: " + properties.messagesPerSecond + " msg/s");
 
             long emitTime = System.currentTimeMillis() - emitStartTime;
             if (emitTime < 1000) {
