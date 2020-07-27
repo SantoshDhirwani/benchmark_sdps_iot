ce35dbdb61fe8cbb0d546335c0a60a5e10e70042
Santosh Dhirwani <37183172+SantoshDhirwani@users.noreply.github.com>
2020-07-27 11:50:46 +0200

    Update system_usage_plot.py

--- system_usage_plot.py
+++ system_usage_plot.py
@@ -10,40 +10,20 @@ import matplotlib
 import matplotlib.pyplot as plt
 import time
 
-prev_sent = 0
-prev_recv = 0
-
 def get_process_info():
     cpu_percent = psutil.cpu_percent(interval=0.5)
     mem_percent = psutil.virtual_memory()
 
     return cpu_percent, mem_percent.percent
 
-def get_net_info():
-    global prev_sent, prev_recv
-
-    net = psutil.net_io_counters(pernic=True, nowrap=True)['ens33']
-    curr_sent = net.bytes_sent
-    curr_recv = net.bytes_recv
-
-    sent = round((curr_sent - prev_sent) / 1024 / 1024, 3)
-    recv = round((curr_recv - prev_recv) / 1024 / 1024, 3)
-
-    prev_sent = curr_sent
-    prev_recv = curr_recv
-
-    return sent, recv
-
-def build_chart(name, cpus, memory,net_sent,net_recv,labels):
+def build_chart(name, cpus, memory,labels):
     print("Chart building...")
 
     fig, ax = plt.subplots(constrained_layout=True)
-    plt.xlabel("Time(minutes:seconds)")
-    plt.ylabel("Percentage of usage(%)")
+    plt.xlabel("Time(minute:second)")
+    plt.ylabel("Percentage of usage")
     ax.plot(labels, cpus, label='CPU%')
     ax.plot(labels, memory, label='Memory%')
-    ax.plot(labels, net_sent, label='Sent(MB)')
-    ax.plot(labels, net_recv, label='Recv(MB)')
     ax.legend()
 
     plt.xticks(rotation=90)
@@ -52,13 +32,26 @@ def build_chart(name, cpus, memory,net_sent,net_recv,labels):
     cwd = os.getcwd()
     plt.savefig(os.path.join(cwd, name))
 
-
-
 def get_data():
     cpus = []
     memory = []
-    net_sent = []
-    net_recv = []
     timestamps = []
-    for x in range(30):
+    for x in range(20):
         cpu, mem = get_process_info()
+        ts = datetime.fromtimestamp(time.time()).strftime("%M:%S")
+        cpus.append(cpu)
+        memory.append(mem)
+        timestamps.append(ts)
+        time.sleep(1)
+        print(cpu,mem,ts)
+
+    return cpus,memory,timestamps
+
+def main():
+    cpus,memory,timestamps = get_data()
+
+    build_chart("sys_usage.png", cpus, memory, timestamps)
+
+
+if __name__ == '__main__':
+    main()
